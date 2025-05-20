fbd94d6241c1962a254c3223cb0982e46ed3ba07
guswn3717 <rudals9973@gmail.com>
2025-05-20 18:44:20 +0900

    HELP!!!!!!

--- Game/Assets/Scripts/ObstacleManager.cs
+++ Game/Assets/Scripts/ObstacleManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    [SerializeField] int createCount = 5;
+    [SerializeField] List<GameObject> obstacles;
+    [SerializeField] GameObject[] prefab;
+    [SerializeField] Transform[] transforms;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Create();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Create()
     {
-        
+        for (int i = 0; i < createCount; i++)
+        {
+            GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)], transform);
+
+            clone.SetActive(false);
+
+            obstacles.Add(clone);
+        }
     }
 }
