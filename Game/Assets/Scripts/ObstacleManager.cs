c662b3c186a8a43e9656a6c5b9f6b23f122cb61b
guswn3717 <rudals9973@gmail.com>
2025-05-20 19:43:19 +0900

    20250520

--- Game/Assets/Scripts/ObstacleManager.cs
+++ Game/Assets/Scripts/ObstacleManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    [SerializeField] int random;
     [SerializeField] int createCount = 5;
     [SerializeField] List<GameObject> obstacles;
     [SerializeField] GameObject[] prefab;
     [SerializeField] Transform[] transforms;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         Create();
+
+        StartCoroutine(ActivateObstacle());
     }
 
     public void Create()
@@ -24,6 +27,28 @@ public class ObstacleManager : MonoBehaviour
             clone.SetActive(false);
 
             obstacles.Add(clone);
+        }
+    }
+    
+    public IEnumerator ActivateObstacle()
+    {
+        while (true)
+        {
+            random = Random.Range(0, obstacles.Count);
+
+            // 현재 게임 오브젝트가 활성화되어 있는 지 확인한다다
+            while (obstacles[random].activeSelf == true)
+            {
+                //현제 인덱스에 있는 게임 오브젝트가 활성화되어 있으면
+                //random 변수의 값을 +1을 해서 다시 검색한다
+                random = (random + 1) % obstacles.Count;
+            }
+
+            obstacles[random].transform.position = transforms[Random.Range(0, transforms.Length)].position;
+
+            obstacles[random].SetActive(true);
+
+            yield return new WaitForSeconds(5);
         }
     }
 }
